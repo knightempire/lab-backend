9d841eff8684affbe241edb04b1265f3a34e901a
Raamprathap <raamprathap17242@gmail.com>
2025-05-07 16:02:47 +0530

    Test mail
    Have created a sample code which sends mail from one end to to user end with custom message

---
+++ middleware/mail/test/test.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+console.log('User:', process.env.BREVO_USER);
+console.log('Pass:', process.env.BREVO_PASS);
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
+  }
+});
+
+const mailOptions = {
+  from: `raamprathap17242@gmail.com`,
+  to: 'postaccidentalertsystem@gmail.com',
+  subject: 'Test Email',
+  text: 'Hello! This is a test email.',
+  html: '<h2>Hello</h2><p>This is a <strong>custom message</strong></p>'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.log('Email failed:', error);
+  }
+  console.log('Email sent:', info.response);
+});
