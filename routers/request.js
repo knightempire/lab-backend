9e4558b209e356713ab49c531d912d67e5736f0c
Raamprathap <raamprathap17242@gmail.com>
2025-05-22 13:26:05 +0530

    getUserRequest
    Have added getUserRequest controller to get the requests applied by a particular user

--- routers/request.js
+++ routers/request.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = express.Router(); 
-const { addRequest, updateRequest, fetchRequest, fetchAllRequests, approveRequest, rejectRequest, fetchUserRequests, fetchRefRequests, fetchRequestByStatus } = require('../controllers/requests.controllers');
+const { addRequest, updateRequest, fetchRequest, fetchAllRequests, approveRequest, rejectRequest, fetchUserRequests, fetchRefRequests, fetchRequestByStatus, getUserRequests} = require('../controllers/requests.controllers');
 const { admintokenValidator, tokenValidator } = require('../middleware/auth/tokenvalidate.js');
 
 request.post('/add', tokenValidator, addRequest);
@@ -12,5 +12,6 @@ request.get('/get/:id', tokenValidator, fetchRequest);
 request.post('/approve/:id', admintokenValidator, approveRequest);
 request.post('/reject/:id', admintokenValidator, rejectRequest);
 request.get('/', admintokenValidator, fetchRequestByStatus);
+request.get('/user', tokenValidator, getUserRequests);
 
 module.exports = request;
