a5df74734b161249bff17c9eeafd31bbff6da7f9
Raamprathap <raamprathap17242@gmail.com>
2025-05-23 12:42:40 +0530

    Modified request model and controllers
    Have modified the requests model by adding scheduledCollectionDate and collectedDate.
    
    Have created a controller to handle collecting products and have modified other controllers to the updated model and have added route for the new controller.

--- routers/request.js
+++ routers/request.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = express.Router(); 
-const { addRequest, updateRequest, fetchRequest, fetchAllRequests, approveRequest, rejectRequest, fetchUserRequests, fetchRefRequests, fetchRequestByStatus, getUserRequests} = require('../controllers/requests.controllers');
+const { addRequest, updateRequest, fetchRequest, fetchAllRequests, approveRequest, rejectRequest, 
+    fetchUserRequests, fetchRefRequests, fetchRequestByStatus, getUserRequests, collectProducts } = require('../controllers/requests.controllers');
 const { admintokenValidator, tokenValidator } = require('../middleware/auth/tokenvalidate.js');
 
 request.post('/add', tokenValidator, addRequest);
@@ -13,5 +14,6 @@ request.post('/approve/:id', admintokenValidator, approveRequest);
 request.post('/reject/:id', admintokenValidator, rejectRequest);
 request.get('/', admintokenValidator, fetchRequestByStatus);
 request.get('/user', tokenValidator, getUserRequests);
+request.post('/collect/:id', tokenValidator, collectProducts);
 
 module.exports = request;
