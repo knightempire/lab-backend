66bd48be3443c1a176256936167f98234dfc6948
Raamprathap <raamprathap17242@gmail.com>
2025-06-27 19:18:04 +0530

    Enhancement

--- routers/user.js
+++ routers/user.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const users = express.Router(); 
 const { fetchAllUsers, adminFetchUser, adminUpdateUser } = require('../controllers/admin/profile.controllers.js');
-const { updateUser, fetchUser, userStats } = require('../controllers/profile.controllers');
+const { updateUser, fetchUser, userStats, getRequestsandDamagedCounts } = require('../controllers/profile.controllers');
 const { admintokenValidator, tokenValidator } = require('../middleware/auth/tokenvalidate.js');
 
 users.put('/update/:rollNo', admintokenValidator, adminUpdateUser);
@@ -10,6 +10,7 @@ users.get('/get', admintokenValidator, fetchAllUsers);
 users.get('/get/:rollNo', admintokenValidator, adminFetchUser);
 users.put('/update', tokenValidator, updateUser);
 users.get('/get-user', tokenValidator, fetchUser);
+users.get('/requests-and-damaged-counts', tokenValidator, getRequestsandDamagedCounts);
 users.get('/stats', tokenValidator, userStats);
 
 module.exports = users;
